aae8233a1183b9c9f465f49e817684ec6ef3b709
rickyruozzi <ruozziriccardo@4gmail.com>
2025-07-09 19:11:37 +0200

    nuovo file C

---
+++ ProveDiVisualizzazione.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("Prove");
+    printf("     di");
+    printf("       visualizzazione");
+}
--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 int main(){
-	printf("Hello World!");
+        printf("Hello World!");
+        int scelta;
+        printf("Scegli quale programma avviare!\n");
+        scanf("%d",&scelta);
+        printf("%d \n",scelta);
+
+        return 0;
 }
+
+
