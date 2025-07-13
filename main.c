470c641d99f750a86a9cd1ae51951377e631e631
rickyruozzi <ruozziriccardo@4gmail.com>
2025-07-13 17:32:12 +0200

    message

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-07-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
@@ -5,7 +15,7 @@ int main() {
     printf("Scegli quale programma avviare!\n");
     printf("1-per le prove di visualizzazione\n2-per il programma che somma 3 uint\n"
     "3-per invertire i valori di un array\n4-per controllare la prima ricorrenza in un valore in un array di interi casuali"
-    "\n5-per invertire una stringa");
+    "\n5-per invertire una stringa\n");
     fflush(stdout);          // svuota il buffer, la stringa appare subito
     int scelta;
     scanf("%d", &scelta);
