74b03b15c914efb019beb6743f02a59aebc3b02c
rickyruozzi <ruozziriccardo@4gmail.com>
2025-07-18 02:09:28 +0200

    roba file

--- sourceFiles/FileFunctions.c
+++ sourceFiles/FileFunctions.c
@@ -23,4 +23,44 @@ int carica_studenti(const char* filename, Studente studenti[], int max){
     fclose(file);
 }
 
+void stampa_studenti(const Studente studenti[], int n){
+    for (int i = 0; i < n; i++) {
+        printf("%s %d\n", studenti[i].nome, studenti[i].età);
+    }
+}
+
+void aggiungi_studente(Studente studenti[], int* n) {
+    if (*n >= 25) {
+        printf("Numero massimo di studenti raggiunto.\n");
+        return;
+    }
+
+    Studente nuovo;
+    printf("Nome completo: ");
+    fgets(nuovo.nome, MAX_L, stdin);
+    nuovo.nome[strcspn(nuovo.nome, "\n")] = 0; // Rimuove newline
+
+    printf("Età: ");
+    scanf("%d", &nuovo.età);
+    getchar(); // pulizia newline
+    studenti[*n] = nuovo;
+    (*n)++;
+
+    printf("Studente aggiunto con successo.\n");
+}
+
+int salva_studenti(const char* filename, Studente studenti[], int n){
+    FILE* file = fopen('../studenti.txt', "r");
+    if (!file) {
+        printf("Errore nell'apertura del file per scrittura.\n");
+        return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        fprintf(file, "%s,%d\n", studenti[i].nome, studenti[i].età);
+    }
+    fclose(file);
+    return 1;
+}
+
 int main(){}
